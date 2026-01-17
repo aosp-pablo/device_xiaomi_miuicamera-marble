265db319b70ecff2c7f57b73f87c5007c5424853
pabloescobar-reborn <yogivangala07@gmail.com>
2026-01-17 05:16:44 +0000

    miuicamera-marble: split camera APK into 20MB chunks
    Co-authored-by: adithya2306 <adithya2306@users.noreply.github.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+cat vendor/xiaomi/miuicamera-marble/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk.part* > vendor/xiaomi/miuicamera-marble/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk
