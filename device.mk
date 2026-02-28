6832d920ae617937b337ff39d6ed73e957b72524
pabloescobar-reborn <yogivangala07@gmail.com>
2026-02-28 13:58:59 +0530

    prebuilts: import miui calculator
    only build wen using gappps

--- device.mk
+++ device.mk
@@ -7,6 +7,12 @@
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/miuicamera-marble/miuicamera-marble-vendor.mk)
 
+# MiuiPrebuilts
+ifneq ($(filter true,$(WITH_GAPPS) $(WITH_GMS)),)
+PRODUCT_PACKAGES += \
+    MiuiCalculator
+endif
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries-xiaomi.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/public.libraries-xiaomi.txt
